2262047a73e29da9ff8790ff7e21231a485429b6
MacaronFR <denisft77@gmail.com>
2021-04-08 19:34:54 +0200

    secure StoreController

--- Controllers/StoreController.php
+++ Controllers/StoreController.php
@@ -8,27 +8,31 @@ class StoreController extends Controller
 {
 
 	/**
+     * Retrieve the first 500 products on /store or the Nth 500 products for /store/N
 	 * @inheritDoc
 	 */
 	public function get(array $args){
 		$pm = new ProductModel();
 		if(count($args['uri_args']) === 0){
-			$products = $pm->selectAll();
-			if($products)
-				response(200, "Product 1 to 500", $products);
-			else
-				response(204, "No product found");
+			$iteration = 0;
 		}elseif(count($args['uri_args']) === 1){
 		    if(is_numeric($args['uri_args'][0])){
-                $products = $pm->selectAll((int)$args['uri_args'][0]);
-                if($products) {
-                    $start = (int)$args['uri_args'][0] * 500 + 1;
-                    $end = ((int)$args['uri_args'][0] + 1 )* 500;
-                    response(200, "Product ${start} to ${end}", $products);
-                }else
-                    response(204, "No product found");
+                $iteration = (int)$args['uri_args'][0];
+            }else{
+		        response(400, "Bad Request");
+		        return;
             }
+        }else{
+            response(400, "Bad Request");
+            return;
         }
+        $products = $pm->selectAll($iteration);
+        if($products) {
+            $start = (int)$args['uri_args'][0] * 500 + 1;
+            $end = ((int)$args['uri_args'][0] + 1 )* 500;
+            response(200, "Product $start to $end", $products);
+        }else
+            response(204, "No product found");
 	}
 
 	/**
