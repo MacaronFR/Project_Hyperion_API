50d4596ac431c09fc0980cee548f641656959753
Retr0 <ngallot@myges.fr>
2021-04-17 20:37:06 +0200

    TypeController.php + other changes

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use \Hyperion\API\{OAuthController,ConnectionController,StoreController,ProfileController,CategoryController};
 use \Hyperion\API\Router;
+use \Hyperion\API\{TypeController};
 
 require_once "autoload.php";
 
@@ -17,13 +18,19 @@ $rt->delete("/disconnect/*", ConnectionController::class);
 // /inscription/{client_token}
 // {}
 $rt->post("/inscription/*", ConnectionController::class);
+//Store
 $rt->get("/store", StoreController::class);
 $rt->get("/store/*", StoreController::class);
+//Profile
 $rt->get("/me/*", ProfileController::class);
+//Category
 $rt->get("/category", CategoryController::class);
 $rt->get("/category/*", CategoryController::class);
 $rt->post("/category/*", CategoryController::class);
 $rt->put("/cat", CategoryController::class);
+//Type
+$rt->get("/category/type/*",TypeController::class);
+$rt->get("/category/type/*/*",TypeController::class);
 if(!$rt->getRouted()){
 	response(404, "Not Found");
 }
