5a0a1ab3665539b48a0db60855dfa0f4549b103d
Retr0 <ngallot@myges.fr>
2021-04-17 15:20:39 +0200

    ContributeModel.php

---
+++ Models/ContributeModel.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Hyperion\API;
+
+
+class ContributeModel extends Model{
+    protected string $id_name = "id_contribute";
+    protected string $table_name = "CONTRIBUTE";
+    protected array $column = [
+        "contributor"=>"id_contributor",
+        "value"=>"value",
+        "project"=>"id_project",
+        "user"=>"user",
+    ];
+}
