8ab31d082ed266862f4bf77e41fc0162c73f611f
Retr0 <ngallot@myges.fr>
2021-04-17 19:01:31 +0200

    SelectByCategory

--- Models/TypeModel.php
+++ Models/TypeModel.php
@@ -8,8 +8,13 @@ class TypeModel extends Model{
     protected string $id_name = "id_type";
     protected string $table_name = "TYPES";
     protected array $column = [
-        "id_type"=>"id_type",
         "type"=>"type",
         "category"=>"category"
     ];
+
+    public function selectByCategory(int $id_category, int $iteration = 0): array|false{
+        $start = $iteration*500;
+       return  $this->prepared_query("SELECT type,category FROM TYPES WHERE category=:id LIMIT $start,500",
+       ["id"=>$id_category]);
+    }
 }
