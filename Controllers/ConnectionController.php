52479de774bdd420c75c7969606cc74c0065d543
MacaronFR <denisft77@gmail.com>
2021-04-18 15:45:43 +0200

    change ConnectionController.php

--- Controllers/ConnectionController.php
+++ Controllers/ConnectionController.php
@@ -30,7 +30,7 @@ class ConnectionController implements Controller{
 		if($clientInfo !== false && $args['uri_args'][1] === $clientInfo['secret']){
 			$user = $this->userM->selectFromMail($args['uri_args'][2]);
 			if($user['password'] === $args['uri_args'][3]){
-				$message = "Location: /token/" . $args['uri_args'][0] . "/" . $args['uri_args'][1] . "/" . $args['uri_args'][2] . "/" . $args['uri_args'][3];
+				$message = "/token/" . $args['uri_args'][0] . "/" . $args['uri_args'][1] . "/" . $args['uri_args'][2] . "/" . $args['uri_args'][3];
 				if(!$this->userM->update($user['id_user'], ['llog' => (new DateTime())->format("Y-m-d H:i:s")]))
 					$message = "Error on updating last_login date";
 				response(302, $message, ['id' => $user['id_user'], 'type' => $user['type'], 'name' => $user['name'], 'mail' => $args['uri_args'][2]]);
