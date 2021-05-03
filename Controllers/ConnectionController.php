7d257cd41ddab6d0f6f9808e1b224b1be4d185b8
MacaronFR <denisft77@gmail.com>
2021-05-03 21:46:32 +0200

    log ConnectionController.php

--- Controllers/ConnectionController.php
+++ Controllers/ConnectionController.php
@@ -9,13 +9,13 @@ use JetBrains\PhpStorm\NoReturn;
 require_once "autoload.php";
 
 class ConnectionController implements Controller{
-	private UserModel $userM;
+	private UserModel $um;
 	private TokenModel $tm;
 	private DateTime $now;
 	private ClientModel $cm;
 
 	public function __construct(){
-		$this->userM = new UserModel();
+		$this->um = new UserModel();
 		$this->tm = new TokenModel();
 		$this->now = new DateTime();
 		$this->cm = new ClientModel();
@@ -27,17 +27,20 @@ class ConnectionController implements Controller{
 	 */
 	#[NoReturn] public function get(array $args){
 		$clientInfo = $this->cm->selectFromClientID($args['uri_args'][0]);
-		if($clientInfo !== false && $args['uri_args'][1] === $clientInfo['secret']){
-			$user = $this->userM->selectFromMail($args['uri_args'][2]);
-			if($user['password'] === $args['uri_args'][3]){
-				$message = "/token/" . $args['uri_args'][0] . "/" . $args['uri_args'][1] . "/" . $args['uri_args'][2] . "/" . $args['uri_args'][3];
-				if(!$this->userM->update($user['id_user'], ['llog' => (new DateTime())->format("Y-m-d H:i:s")]))
-					$message = "Error on updating last_login date";
-				response(302, $message, ['id' => $user['id_user'], 'type' => $user['type'], 'name' => $user['name'], 'mail' => $args['uri_args'][2]]);
-			}else{
-				response(403, "Unauthorized access");
-			}
+		if($clientInfo === false || $args['uri_args'][1] !== $clientInfo['secret']){
+			response(403, "Forbidden");
 		}
+		$user = $this->um->selectFromMail($args['uri_args'][2]);
+		if($user === false){
+			response(500, "Internal Server Error");
+		}
+		if($user['password'] !== $args['uri_args'][3]){
+			response(403, "Forbidden");
+		}
+		if(!$this->um->update($user['id_user'], ['llog' => (new DateTime())->format("Y-m-d H:i:s")]))
+			response(500, "Internal Server Error");
+		$message = "/token/" . $args['uri_args'][0] . "/" . $args['uri_args'][1] . "/" . $args['uri_args'][2] . "/" . $args['uri_args'][3];
+		response(302, $message, ['id' => $user['id_user'], 'type' => $user['type'], 'name' => $user['name'], 'mail' => $args['uri_args'][2]]);
 	}
 
 	/**
@@ -45,30 +48,26 @@ class ConnectionController implements Controller{
 	 * @inheritDoc
 	 */
 	#[NoReturn] public function post(array $args){
-		$token = $this->tm->selectByToken($args['uri_args'][0]);
-		if($token !== false && ((int)$token['scope']) === 0){
-			$values = $args['post_args'];
-			if(isset($values['name'], $values['fname'], $values['mail'], $values['passwd']) && count($values) === 4){
-				if($this->userM->selectFromMail($values['mail']) !== false){
-					response(400, "Mail already exist");
-				}
-				$values['gc'] = 0;
-				$values['type'] = 4;
-
-				$values['llog'] = $this->now->format("Y-m-d H:i:s");
-				$values['ac_creation'] = $this->now->format("Y-m-d H:i:s");
-				$values['addr'] = null;
-				if($this->userM->insert($values)){
-					response(201, "User Created");
-				}else{
-					response(500, "Error creating user");
-				}
-			}else{
-				response(400, "Bad Request, mismatch in given argument");
-			}
-		}else{
+		if(!checkToken($args['uri_args'][0], 1)){
 			response(401, "Invalid Credentials");
 		}
+		$values = $args['post_args'];
+		if(!isset($values['name'], $values['fname'], $values['mail'], $values['passwd']) || count($values) !== 4){
+			response(400, "Bad Request");
+		}
+		if($this->um->selectFromMail($values['mail']) !== false){
+			response(400, "Mail already exist");
+		}
+		$values['gc'] = 0;
+		$values['type'] = 4;
+		$values['llog'] = $this->now->format("Y-m-d H:i:s");
+		$values['ac_creation'] = $this->now->format("Y-m-d H:i:s");
+		$values['addr'] = null;
+		if($this->um->insert($values)){
+			API_log($args['uri_args'][0], "USERS", "Creating user");
+			response(201, "User Created");
+		}
+		response(500, "Error creating user");
 	}
 
 	/**
@@ -84,14 +83,13 @@ class ConnectionController implements Controller{
 	 */
 	#[NoReturn] public function delete(array $args){
 		$token_id = $this->tm->selectByToken($args["uri_args"][0]);
-		if($token_id !== false){
-			if($this->tm->delete($token_id['id'])){
-				response(200, "Disconnected");
-			}else{
-				response(500, "Error during disconnection");
-			}
-		}else{
+		if($token_id === false){
 			response(404, "User token not found");
 		}
+		if($this->tm->delete($token_id['id'])){
+			API_log($args['uri_args'][0], "USERS", "Disconnect User");
+			response(200, "Disconnected");
+		}
+		response(500, "Error during disconnection");
 	}
 }
