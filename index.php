2ed4360d17759103b379fb90de8072acf398edf9
MacaronFR <denisft77@gmail.com>
2021-05-05 00:34:10 +0200

    big refactor on route and doc

--- index.php
+++ index.php
@@ -12,10 +12,8 @@ if($_SERVER['REQUEST_METHOD'] === "OPTIONS"){
 }
 
 $rt = new Router();
-// /token/{client_id}/{client_secret}/{user_mail}/{user_passwd} => user token
-$rt->get("/token/*/*/*/*", OAuthController::class);
-// /token/{client_id}/{client_secret} => client token
-$rt->get("/token/*/*", OAuthController::class);
+// /token/{client_id}/{client_secret}[/{user_mail}/{user_passwd}]
+$rt->get("/token/*/*{/*/*}", OAuthController::class);
 // /connect/{client_id}/{client_secret}/{user_mail}/{user_passwd}
 $rt->get("/connect/*/*/*/*", ConnectionController::class);
 // /disconnect/{user_token}
@@ -25,8 +23,7 @@ $rt->delete("/disconnect/*", ConnectionController::class);
 $rt->post("/inscription/*", ConnectionController::class);
 //Store
 // /store[/{page}]
-$rt->get("/store", StoreController::class);
-$rt->get("/store/*", StoreController::class);
+$rt->get("/store{/*}", StoreController::class);
 //Profile
 // /me/{user_token}
 $rt->get("/me/*", ProfileController::class);
@@ -57,57 +54,43 @@ $rt->post("/type/*", TypeController::class);
 // /category/type/{id_category}[/{page}]
 $rt->get("/category/*/type{/*}",MarkModelController::class, ["type"]);
 // /type/{id_type}/product[/{page}]
-$rt->get("/type/*/product", ProductHierarchyController::class, ["type_product"]);
-$rt->get("/type/*/product/*", ProductHierarchyController::class, ["type_product"]);
+$rt->get("/type/*/product{/*}", ProductHierarchyController::class, ["type_product"]);
 // /type/{id_type}/reference[/{page}]
-$rt->get("/type/*/reference", ReferenceHierarchyController::class, ["type_reference"]);
-$rt->get("/type/*/reference/*", ReferenceHierarchyController::class, ["type_reference"]);
+$rt->get("/type/*/reference{/*}", ReferenceHierarchyController::class, ["type_reference"]);
 // /mark/{mark_name}/product[/{page}]
-$rt->get("/mark/*/product", ProductHierarchyController::class, ["mark_product"]);
-$rt->get("/mark/*/product/*", ProductHierarchyController::class, ["mark_product"]);
+$rt->get("/mark/*/product{/*}", ProductHierarchyController::class, ["mark_product"]);
 // /mark/{mark_name}/reference[/{page}]
-$rt->get("/mark/*/reference", ReferenceHierarchyController::class, ["mark_reference"]);
-$rt->get("/mark/*/reference/*", ReferenceHierarchyController::class, ["mark_reference"]);
+$rt->get("/mark/*/reference{/*}", ReferenceHierarchyController::class, ["mark_reference"]);
 // /type/{id_type}/mark/{mark_name}/product[/{page}]
-$rt->get("/type/*/mark/*/product", ProductHierarchyController::class, ["type_mark_product"]);
-$rt->get("/type/*/mark/*/product/*", ProductHierarchyController::class, ["type_mark_product"]);
+$rt->get("/type/*/mark/*/product{/*}", ProductHierarchyController::class, ["type_mark_product"]);
 // /type/{id_type}/mark/{mark_name}/reference[/{page}]
-$rt->get("/type/*/mark/*/reference", ReferenceHierarchyController::class, ["type_mark_reference"]);
-$rt->get("/type/*/mark/*/reference/*", ReferenceHierarchyController::class, ["type_mark_reference"]);
-
-$rt->get("/type/*/mark", MarkModelController::class, ["type_mark"]);
-$rt->get("/type/*/mark/*", MarkModelController::class, ["type_mark"]);
-
+$rt->get("/type/*/mark/*/reference{/*}", ReferenceHierarchyController::class, ["type_mark_reference"]);
+// /type/{id_type}/mark[/{page}]
+$rt->get("/type/*/mark{/*}", MarkModelController::class, ["type_mark"]);
+// /mark/{mark_name}/model[/{page}]
 $rt->get("/mark/*/model{/*}", MarkModelController::class, ["mark_model"]);
-
+// /type/{id_type}/mark/{mark_name}/model[/{page}]
 $rt->get("/type/*/mark/*/model{/*}", MarkModelController::class, ["type_mark_model"]);
-
+// /mark[/{page}]
 $rt->get("/mark{/*}", MarkModelController::class, ["mark"]);
-
-$rt->get("/model", MarkModelController::class, ["model"]);
-$rt->get("/model/*", MarkModelController::class, ["model"]);
-
-$rt->get("/model/*/product", ProductHierarchyController::class, ['model_product']);
-$rt->get("/model/*/product/*", ProductHierarchyController::class, ['model_product']);
-
-$rt->get("/mark/*/model/*/product", ProductHierarchyController::class, ['mark_model_product']);
-$rt->get("/mark/*/model/*/product/*", ProductHierarchyController::class, ['mark_model_product']);
-
-$rt->get("/type/*/model/*/product", ProductHierarchyController::class, ['type_model_product']);
-$rt->get("/type/*/model/*/product/*", ProductHierarchyController::class, ['type_model_product']);
-
-$rt->get("/type/*/mark/*/model/*/product", ProductHierarchyController::class, ['type_mark_model_product']);
-$rt->get("/type/*/mark/*/model/*/product/*", ProductHierarchyController::class, ['type_mark_model_product']);
-
+// /model[/{page}]
+$rt->get("/model{/*}", MarkModelController::class, ["model"]);
+// /model/{model_name}/product[/{page}]
+$rt->get("/model/*/product{/*}", ProductHierarchyController::class, ['model_product']);
+// /mark/{mark_name}/model/{model_name}/product[/{page}]
+$rt->get("/mark/*/model/*/product{/*}", ProductHierarchyController::class, ['mark_model_product']);
+// /type/{type_id}/model/{model_name}/product/[/{page}]
+$rt->get("/type/*/model/*/product{/*}", ProductHierarchyController::class, ['type_model_product']);
+// /type/{type_id}/mark/{mark_name}/model/{model_name}/product[/{page}]
+$rt->get("/type/*/mark/*/model/*/product{/*}", ProductHierarchyController::class, ['type_mark_model_product']);
+// /model/{model_name}/reference
 $rt->get("/model/*/reference", ReferenceHierarchyController::class, ['model_reference']);
-
+// /mark/{mark_name}/model/{model_name}/reference
 $rt->get("/mark/*/model/*/reference", ReferenceHierarchyController::class, ['mark_model_reference']);
-
+// /type/{type_id}/model/{model_name}/reference
 $rt->get("/type/*/model/*/reference", ReferenceHierarchyController::class, ['type_model_reference']);
-
+// /type/{type_id}/mark/{mark_name}/model/{model_name}/reference
 $rt->get("/type/*/mark/*/model/*/reference", ReferenceHierarchyController::class, ['type_mark_model_reference']);
-
-// Specification route
 // /specification[/{page}[/search/{search}[/order/{direction}/sort{column}]]]
 $rt->get("/specification{/*{/search/*{/order/*/sort/*}}}",SpecController::class);
 $rt->get("/specification/name{/*}",SpecController::class, ['name']);
@@ -117,9 +100,10 @@ $rt->put("/specification/*/*",SpecController::class);
 $rt->delete("/specification/*/*",SpecController::class);
 // /specification/{token}
 $rt->post('/specification/*',SpecController::class);
-
-// /specification/{token}
+// /reference/{token}
 $rt->post('/reference/*', ReferenceHierarchyController::class);
 if(!$rt->getRouted()){
 	response(404, "Not Found");
 }
+
+//TODO add /tye/*/model{/*} route
