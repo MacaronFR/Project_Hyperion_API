5bcee951ae1eabe548f74b8ea82af8393484667a
MacaronFR <denisft77@gmail.com>
2021-06-22 14:17:06 +0200

    test user 5

--- Controllers/ProfileController.php
+++ Controllers/ProfileController.php
@@ -112,6 +112,7 @@ class ProfileController implements Controller{
 		$user_update = array_intersect_key($args['put_args'], $user_info);
 		var_dump($user_update, $user_info, $args['put_args']);
 		if(!empty($user_update)){
+			var_dump("ici");
 			if($this->um->update($user_info['id'], $user_update)){
 				response(200, "Profile Updated");
 			}else{
